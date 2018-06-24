6231e51b718a10410cb77decdeb7e18545cace23
Felipe Murakami <fhmurakami@gmail.com>
2018-06-24 20:17:40 -0300

    Add JavaScript - Fundamentos - numerosAlgunsCuidados.js

---
+++ 01-JavaScript/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0) // Retorna Infinity
+console.log("10" / 2) // Divisão de String por numero
+console.log("Texto" * 2) // Retorna NaN
+console.log(0.1 + 0.7) // Não retorna 0.8, a especificação para maior precisão é mais lenta.
+// console.log(10.toString()) // Esta linha gera um erro de sintaxe. Deve ser utilizado da forma abaixo:
+console.log((10).toString())
