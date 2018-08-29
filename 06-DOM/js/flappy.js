bab36d95ef3807610f5ada35961afa55430ae35b
Felipe Murakami <fhmurakami@gmail.com>
2018-08-29 13:28:10 -0300

    Add Integrando HTML, CSS e JS - Flappy Bird 04

--- 06-DOM/js/flappy.js
+++ 06-DOM/js/flappy.js
@@ -74,9 +74,42 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
     }
 }
 
+function Passaro(alturaJogo) {
+    let voando = false
+
+    this.elemento = novoElemento('img', 'passaro')
+    this.elemento.src = 'imgs/passaro.png'
+
+    this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
+    this.setY = y => this.elemento.style.bottom = `${y}px`
+
+    window.onkeydown = e => voando = true
+    window.onkeyup = e => voando = false
+
+    this.animar = () => {
+        const novoY = this.getY() + (voando ? 8 : -5)
+        const alturaMaxima = alturaJogo - this.elemento.clientHeight
+
+        if (novoY <= 0) {
+            this.setY(0)
+        } else if (novoY >= alturaMaxima) {
+            this.setY(alturaMaxima)
+        } else {
+            this.setY(novoY)
+        }
+    }
+
+    this.setY(alturaJogo / 2)
+}
+
 const barreiras = new Barreiras(500, 1200, 200, 400)
 const areaDoJogo = document.querySelector('[wm-flappy]')
 barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+
+const passaro = new Passaro(500)
+areaDoJogo.appendChild(passaro.elemento)
+
 setInterval(() => {
     barreiras.animar()
+    passaro.animar()
 }, 20)
