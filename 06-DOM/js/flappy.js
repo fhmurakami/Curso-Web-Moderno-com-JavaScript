fb7174c725d3a2ec2f100a254e6e09e059d00f2e
Felipe Murakami <fhmurakami@gmail.com>
2018-08-29 16:20:56 -0300

    Add Integrando HTML, CSS e JS - Flappy Bird 05

--- 06-DOM/js/flappy.js
+++ 06-DOM/js/flappy.js
@@ -102,14 +102,48 @@ function Passaro(alturaJogo) {
     this.setY(alturaJogo / 2)
 }
 
-const barreiras = new Barreiras(500, 1200, 200, 400)
-const areaDoJogo = document.querySelector('[wm-flappy]')
-barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+// const barreiras = new Barreiras(500, 1200, 200, 400)
+// const areaDoJogo = document.querySelector('[wm-flappy]')
+// barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
 
-const passaro = new Passaro(500)
-areaDoJogo.appendChild(passaro.elemento)
+// const passaro = new Passaro(500)
+// areaDoJogo.appendChild(passaro.elemento)
 
-setInterval(() => {
-    barreiras.animar()
-    passaro.animar()
-}, 20)
+// setInterval(() => {
+//     barreiras.animar()
+//     passaro.animar()
+// }, 20)
+
+function Progresso() {
+    this.elemento = novoElemento('span', 'progresso')
+    this.atualizarPontos = pontos => {
+        this.elemento.innerHTML = pontos
+    }
+    this.atualizarPontos(0)
+}
+
+function FlappyBird() {
+    let pontos = 0
+
+    const areaDoJogo = document.querySelector('[wm-flappy]')
+    const altura = areaDoJogo.clientHeight
+    const largura = areaDoJogo.clientWidth
+
+    const progresso = new Progresso()
+    const barreiras = new Barreiras(altura, largura, 200, 400, () => progresso.atualizarPontos(++pontos))
+    const passaro = new Passaro(altura)
+
+    areaDoJogo.appendChild(progresso.elemento)
+    areaDoJogo.appendChild(passaro.elemento)
+    barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+
+    this.start = () => {
+        //loop do jogo
+        const temporizador = setInterval(() => {
+            barreiras.animar()
+            passaro.animar()
+        }, 20)
+    }
+}
+
+new FlappyBird().start()
