e373d77a114f2de292b58584d03ef1651bf5303b
Felipe Murakami <fhmurakami@gmail.com>
2018-09-08 12:05:44 -0300

    Add Webpack - Carregando Arquivo .SCSS (SASS)

--- 09-Webpack/webpack/webpack.config.js
+++ 09-Webpack/webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: 'development',
@@ -6,5 +7,21 @@ module.exports = {
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader, // Gera conflito com o 'style-loader'
+                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                'css-loader', // Interpreta @import, url()...
+                'sass-loader'
+            ]
+        }]
     }
 }
