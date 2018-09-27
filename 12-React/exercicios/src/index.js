73a0f787e231b87b5cb02682d4454a2d3e2e8247
Felipe Murakami <fhmurakami@gmail.com>
2018-09-27 16:47:09 -0300

    Add React - Componentes: Pai & Filho 02

--- 12-React/exercicios/src/index.js
+++ 12-React/exercicios/src/index.js
@@ -8,11 +8,14 @@ import ReactDOM from 'react-dom'
 // import Saudacao from './componentes/Saudacao'
 
 import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
     <div>
         <Pai nome="Paulo" sobrenome="Silva">
-            {/* Como passo os componentes Filhos aqui? */}
+            <Filho nome="Pedro" />
+            <Filho nome="Paulo" />
+            <Filho nome="Carla" />
         </Pai>
     </div>
 , document.getElementById('root'))
