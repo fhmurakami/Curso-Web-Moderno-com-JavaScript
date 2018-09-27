2003ef0201318f9bcc16e233a35feb02a74f7449
Felipe Murakami <fhmurakami@gmail.com>
2018-09-27 10:37:34 -0300

    Add React - Múltiplos Componentes

--- 12-React/exercicios/src/index.js
+++ 12-React/exercicios/src/index.js
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // import Primeiro from './componentes/Primeiro'
-import BomDia from './componentes/BomDia'
+// import BomDia from './componentes/BomDia'
 
-ReactDOM.render(<BomDia nome="Guilherme" />, document.getElementById('root'))
+import Multi, { BoaNoite } from './componentes/Multiplos'
+
+ReactDOM.render(
+    <div>
+        <Multi.BoaTarde nome="Ana" />
+        <BoaNoite nome="Bia" />
+    </div>
+, document.getElementById('root'))
