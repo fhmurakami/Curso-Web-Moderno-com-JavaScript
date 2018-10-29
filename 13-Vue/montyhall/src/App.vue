668daf5e01fd17bd8a95fb50dd7e033c8dccea6d
Felipe Murakami <fhmurakami@gmail.com>
2018-10-29 16:51:29 -0300

    Add Projeto Monty Hall (Vue) - Problema de Monty Hall 04

--- 13-Vue/montyhall/src/App.vue
+++ 13-Vue/montyhall/src/App.vue
@@ -1,7 +1,25 @@
 <template>
     <div id="app">
         <h1>Problema de Monty Hall</h1>
-        <Door number="1" :hasGift="false" />
+        <div class="form">
+            <div v-if="!started">
+                <label for="doorsAmount">Quantas portas? </label>
+                <input type="text" id="doorsAmount" size="3"
+                    v-model.number="doorsAmount">
+            </div>
+            <div v-if="!started">
+                <label for="selectedDoor">Qual porta é premiada? </label>
+                <input type="text" id="selectedDoor" size="3"
+                    v-model.number="selectedDoor">
+            </div>
+            <button v-if="!started" @click="started = true">Iniciar</button>
+            <button v-if="started" @click="started = false">Reiniciar</button>
+        </div>
+        <div class="doors" v-if="started">
+            <div v-for="i in doorsAmount" :key="i">
+                <Door :hasGift="i === selectedDoor" :number="i" />
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,7 +28,14 @@ import Door from './components/Door'
 
 export default {
     name: 'App',
-    components: { Door }
+    components: { Door },
+    data: function() {
+        return {
+            started: false,
+            doorsAmount: 3,
+            selectedDoor: null
+        }
+    }
 }
 </script>
 
@@ -36,5 +61,25 @@ export default {
         background-color: #0004;
         padding: 20px;
         margin-bottom: 60px;
+    }
+
+    .form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 40px;
+    }
+
+    .form, .form input, .form button {
+        margin-bottom: 10px;
+        font-size: 2rem;
+    }
+
+    .doors {
+        align-self: stretch;
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
     }
 </style>
