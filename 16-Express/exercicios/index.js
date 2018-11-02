089c830993e61c6d1498c4c2dc448f3a2150badd
Felipe Murakami <fhmurakami@gmail.com>
2018-11-02 14:04:09 -0300

    Add Express - Tipos de Resposta

--- 16-Express/exercicios/index.js
+++ 16-Express/exercicios/index.js
@@ -2,7 +2,31 @@ const express = require('express')
 const app = express()
 
 app.get('/ola', (req, res) => {
-    res.send('Estou <b>bem</b>!')
+    // res.send('<h1>Estou bem!</h1><br><br><h2>Tipo: HTML</h2>')
+    
+    // res.json({
+    //     name: 'iPad 32 Gb',
+    //     price: 1899.00,
+    //     discount: 0.12
+    // })
+
+    // res.json([
+    //     {id: 7, name: 'Ana', position: 1},
+    //     {id: 34, name: 'Bia', position: 2},
+    //     {id: 73, name: 'Carlos', position: 3}
+    // ])
+
+    res.json({
+        data: [
+            {id: 7, name: 'Ana', position: 1},
+            {id: 34, name: 'Bia', position: 2},
+            {id: 73, name: 'Carlos', position: 3}
+        ],
+        count: 30,
+        skip: 0,
+        limit: 3,
+        status: 200
+    })
 })
 
 app.listen(3000, () => {
