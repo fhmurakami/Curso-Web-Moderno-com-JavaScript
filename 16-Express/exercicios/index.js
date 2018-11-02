d9b92095a27975453bd56abd09ae2f0ab938c237
Felipe Murakami <fhmurakami@gmail.com>
2018-11-02 13:51:02 -0300

    Add Express - Tratando a Primeira Requisição

--- 16-Express/exercicios/index.js
+++ 16-Express/exercicios/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/ola', (req, res) => {
+    res.send('Estou <b>bem</b>!')
+})
+
 app.listen(3000, () => {
     console.log('Backend executando...')
 })
