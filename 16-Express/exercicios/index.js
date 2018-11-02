d98a1759c8597c5bd09104c5067f077b411177a3
Felipe Murakami <fhmurakami@gmail.com>
2018-11-02 14:14:55 -0300

    Add Express - Middleware 01

--- 16-Express/exercicios/index.js
+++ 16-Express/exercicios/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.get('/ola', (req, res) => {
+app.use((req,res, next) => {
+    console.log('Antes do GET')
+    next()
+})
+
+app.get('/ola', (req, res, next) => {
+    console.log('Durante o GET')
     // res.send('<h1>Estou bem!</h1><br><br><h2>Tipo: HTML</h2>')
     
     // res.json({
@@ -27,6 +33,12 @@ app.get('/ola', (req, res) => {
         limit: 3,
         status: 200
     })
+
+    next()
+})
+
+app.use((req,res) => {
+    console.log('Depois do GET')
 })
 
 app.listen(3000, () => {
