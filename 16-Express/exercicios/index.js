de4ae2bb53aa7fbd4ea5050c9deffcc57c3a0a79
Felipe Murakami <fhmurakami@gmail.com>
2018-11-02 16:41:53 -0300

    Add Express - Entendendo o Body Parser

--- 16-Express/exercicios/index.js
+++ 16-Express/exercicios/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 const saudacao = require('./saudacaoMid')
+
+app.use(bodyParser.text())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(saudacao('Guilherme'))
 
@@ -15,14 +20,15 @@ app.get('/clientes/relatorio', (req, res) => {
 })
 
 app.post('/corpo', (req, res) => {
-    let corpo = ''
-    req.on('data', function(parte) {
-        corpo += parte
-    })
+    // let corpo = ''
+    // req.on('data', function(parte) {
+    //     corpo += parte
+    // })
 
-    req.on('end', function() {
-        res.send(corpo)
-    })
+    // req.on('end', function() {
+    //     res.send(corpo)
+    // })
+    res.send(req.body)
 })
 
 app.get('/clientes/:id', (req, res) => {
