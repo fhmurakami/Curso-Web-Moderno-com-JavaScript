dacbcb418f25af5bec98dd7f77a16d84a8e0f8ca
Felipe Murakami <fhmurakami@gmail.com>
2018-11-06 01:09:35 -0200

    Add Projeto Final (Backend) - Informações de Conexão no .env

--- 17-ProjetoFinal/backend/knexfile.js
+++ 17-ProjetoFinal/backend/knexfile.js
@@ -1,12 +1,9 @@
 // Update with your config settings.
+const { db } = require('./.env')
 
 module.exports = {
   client: 'postgresql',
-  connection: {
-    database: 'knowledge',
-    user:     'postgres',
-    password: '061118Set'
-  },
+  connection: db,
   pool: {
     min: 2,
     max: 10
